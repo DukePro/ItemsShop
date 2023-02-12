e4b9a9d9cc95fb3e5ef0e94cbea0566a8368363d
D Egor <Mail.egor.d@gmail.com>
2023-02-12 22:22:51 +0300

    +

--- ItemsShop/Program.cs
+++ ItemsShop/Program.cs
@@ -201,16 +201,16 @@
 
     class Item
     {
-        public int Id { get; private set; }
-        public string Name { get; private set; }
-        public int Price { get; private set; }
-
         public Item(int id, string name, int price)
         {
             Id = id;
             Name = name;
             Price = price;
         }
+ 
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Price { get; private set; }
 
         public void ShowItemInfo(Item item)
         {
